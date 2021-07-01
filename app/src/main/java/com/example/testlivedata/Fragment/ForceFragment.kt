98356f835ad70f78d1b274e4208dbbf6b524dadc
山本 貴就 <t-yamamoto@i-enter.co.jp>
2021-07-01 10:16:26 +0900

    Navigation実装

--- app/src/main/java/com/example/testlivedata/Fragment/ForceFragment.kt
+++ app/src/main/java/com/example/testlivedata/Fragment/ForceFragment.kt
@@ -1,14 +1,17 @@
-package com.example.testlivedata
+package com.example.testlivedata.Fragment
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatTextView
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.navArgs
+import com.example.testlivedata.R
 
 class ForceFragment : Fragment(R.layout.fragment_force) {
+
+    private val args: ForceFragmentArgs by navArgs()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -19,7 +22,9 @@ class ForceFragment : Fragment(R.layout.fragment_force) {
                 findNavController().navigate(action)
             }
         }
+        view.findViewById<AppCompatTextView>(R.id.messageView).apply {
+            text = args.transitionData?.message
 
+        }
     }
-
 }
